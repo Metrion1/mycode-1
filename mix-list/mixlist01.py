231cd96e7aefc8c6cfb35c291f1c68bb1b352195
ProfessorGoon <pfau.walter@gmail.com>
2023-02-13 23:43:59 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,8 @@
+#!/user/bin/env python3
+# Create a list with three items.
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# Display the IP addresses in the list.
+print("The IP addresses in this list are: " + iplist[3] + " and " + iplist[4])
+
+
