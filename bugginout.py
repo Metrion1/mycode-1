b6733546caab6f9221974988dc87c400a4ea7a94
ProfessorGoon <pfau.walter@gmail.com>
2023-02-14 16:37:10 +0000

    Valentine's day problem

---
+++ bugginout.py
@@ -0,0 +1,21 @@
+#1/usr/bin/python3
+#"""Just a lil' warmup!""
+
+def main():
+    #Prompt the user with two questions.
+    name = input("What is your name?\n>")
+    num = input("Pick a number between 1 and 3\n>")
+    num = int(num)
+    num = num - 1
+    
+    #Pick an adjective based on user input.
+    adj_list = ["stupendous","splendiferous","magnificent"]
+    adj = adj_list[num]
+    
+    print("Hello " + name + "!" + " Have a " + adj + " Valentine's day!")
+main()
+
+
+# FINAL PART OF CHALLENGE:
+# USE THE name AND adj VARIABLES TO PRINT THE FOLLOWING:
+# "Hello <name>! Have a <adj> Valentine's Day!
